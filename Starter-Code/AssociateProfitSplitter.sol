967c7e6c9a1979f9e4f1dd397e9e65c3494fa589
YOUR_USERNAME <75440580+valleriia@users.noreply.github.com>
2021-05-20 20:16:28 -0400

    commit

--- Starter-Code/AssociateProfitSplitter.sol
+++ Starter-Code/AssociateProfitSplitter.sol
@@ -1,32 +1,43 @@
 pragma solidity ^0.5.0;
 
-// lvl 1: equal split
 contract AssociateProfitSplitter {
-    // @TODO: Create three payable addresses representing `employee_one`, `employee_two` and `employee_three`.
+    // Create three variables to assign as employees
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;
 
-    constructor(address payable _one, address payable _two, address payable _three) public {
+    // Construct addresses from variables for dynamic addressing
+    constructor(
+        address payable _one,
+        address payable _two,
+        address payable _three
+    ) public {
         employee_one = _one;
         employee_two = _two;
         employee_three = _three;
     }
 
-    function balance() public view returns(uint) {
+    // Check the balance available, which should be 0 after transaction
+    function balance() public view returns (uint256) {
         return address(this).balance;
     }
 
+    //
     function deposit() public payable {
-        // @TODO: Split `msg.value` into three
-        uint amount = ; // Your code here!
+        // Divide
+        uint256 amount = msg.value / 3;
 
-        // @TODO: Transfer the amount to each employee
-        // Your code here!
+        //Transfer amount to each employee
+        employee_one.transfer(amount);
+        employee_two.transfer(amount);
+        employee_three.transfer(amount);
 
-        // @TODO: take care of a potential remainder by sending back to HR (`msg.sender`)
-        // Your code here!
+        // Send remainder to msg.sender
+        msg.sender.transfer(msg.value - (amount * 3));
     }
 
     function() external payable {
-        // @TODO: Enforce that the `deposit` function is called in the fallback function!
-        // Your code here!
+        // Ensure the contract can receive funds from external accounts
+        deposit();
     }
 }
