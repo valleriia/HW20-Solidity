f424ccad2efe378cb604a9b8076f9c60ca9d9bd8
YOUR_USERNAME <75440580+valleriia@users.noreply.github.com>
2021-05-20 22:53:26 -0400

    code

--- Starter-Code/TieredProfitSplitter.sol
+++ Starter-Code/TieredProfitSplitter.sol
@@ -1,38 +1,47 @@
 pragma solidity ^0.5.0;
 
-// lvl 2: tiered split
+// Defining payable accounts
 contract TieredProfitSplitter {
-    address payable employee_one; // ceo
-    address payable employee_two; // cto
-    address payable employee_three; // bob
+    address payable employee_one;
+    address payable employee_two;
+    address payable employee_three;
 
+// Use constructore for dynamic addressability
     constructor(address payable _one, address payable _two, address payable _three) public {
         employee_one = _one;
         employee_two = _two;
         employee_three = _three;
     }
 
-    // Should always return 0! Use this to test your `deposit` function's logic
+// Check balance on contract, should ALWAYS be zero after distribution
     function balance() public view returns(uint) {
         return address(this).balance;
     }
 
+// Percentage split between three employees and the remainder gets sent to employee_one (highest %). 
     function deposit() public payable {
-        uint points = msg.value / 100; // Calculates rudimentary percentage by dividing msg.value into 100 units
+
+// Divide incoming payment (msg.value) into 100 units for easy percentage calculation
+        uint points = msg.value / 100;
         uint total;
         uint amount;
+        
+        amount = points * 60;
+        total += amount;
+        employee_one.transfer(amount);
+        
+        amount = points * 25;
+        total += amount;
+        employee_two.transfer(amount);
+        
+        amount = points * 15;
+        total += amount;
+        employee_three.transfer(amount);
 
-        // @TODO: Calculate and transfer the distribution percentage
-        // Step 1: Set amount to equal `points` * the number of percentage points for this employee
-        // Step 2: Add the `amount` to `total` to keep a running total
-        // Step 3: Transfer the `amount` to the employee
-
-        // @TODO: Repeat the previous steps for `employee_two` and `employee_three`
-        // Your code here!
-
-        employee_one.transfer(msg.value - total); // ceo gets the remaining wei
+        employee_one.transfer(msg.value - total);
     }
 
+// Make sure contract can receive funds from external accounts 
     function() external payable {
         deposit();
     }
